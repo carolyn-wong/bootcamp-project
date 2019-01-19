2331edf4b1a38664194254aef0a7e282bf4b1f59
Carolyn Wong <carolynwong@college.harvard.edu>
2019-01-18 21:08:41 -0500

    modified typeDefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,57 +1,101 @@
 const gql = require('graphql-tag')
 
 module.exports = gql`
-  type Query {
-    user(id: ID!): User!
-    users(
-      substr: String
-      hometown: String
-      house: String
-      concentration: String
-    ): [User!]
-    post(id: ID!): Post!
-    posts: [Post!]
-  }
-
-  type Mutation {
-    createUser(input: CreateUserInput!): LoginReturn!
-    createPost(content: String!): CreatePostReturn!
-    editPost(id: ID!, newContent: String!): EditPostReturn!
-    loginUser(email: String!, password: String!): LoginReturn!
-  }
-
-  type CreatePostReturn {
-    post: Post
-    error: Error
-  }
-
-  type EditPostReturn {
-    post: Post
-    error: Error
-  }
-
-  input CreateUserInput {
-    name: String!
-    email: String!
-    password: String!
-    birthday: String
-    concentration: String
-    hometown: String
-    house: String
-    gender: String
-    bio: String
-    picture: String
-    hobbies: [HobbyInput!]
-  }
-
-  input HobbyInput {
-    hobby: String!
+  enum HOBBY {
+    SPORTS
+    ARTS
+    MUSIC
+    READING
+    TRAVEL
+    DINING
+    CODING
   }
 
   type User {
     id: ID!
     name: String!
     email: String!
+    password: String!
+    birthday: String!
+    concentration: String!
+    hometown: String!
+    house: String!
+    gender: String!
+    bio: String!
+    picture: String!
+    post: [Post]
+    hobby: [Hobby]
+    friend: [Friend]
+  }
+
+  type Post {
+    id: ID!
+    content: String!
+    userId: ID!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Hobby {
+    id: ID!
+    userId: ID!
+    hobby: HOBBY!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Friend {
+    id: ID!
+    senderId: ID!
+    recipientId: ID!
+  }
+
+  input HobbyInput {
+    hobby: HOBBY
+  }
+
+  type Query {
+    users(
+      name: String
+      hometown: String
+      house: String
+      concentration: String
+      hobbies: [HobbyInput]
+    ): [User!]!
+    user(id: ID!): User!
+    post(id: ID!): Post!
+    posts: [Post!]!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): CreateUserReturn!
+    updateUser(input: UpdateUserInput!): UpdateUserReturn!
+    loginUser(input: LoginUserInput!): LoginUserReturn!
+    createPost(input: CreatePostInput!): CreatePostReturn!
+    updatePost(id: ID!, input: UpdatePostInput!): UpdatePostReturn!
+    acceptFriendRequest(
+      requesterId: ID!
+      input: FriendRequestInput
+    ): FriendRequestReturn!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    password: String!
+    createdAt: String!
+  }
+
+  type CreateUserReturn {
+    error: String
+    user: User
+    id: ID
+  }
+
+  input UpdateUserInput {
+    name: String
+    email: String
+    password: String
     birthday: String
     concentration: String
     hometown: String
@@ -59,20 +103,60 @@ module.exports = gql`
     gender: String
     bio: String
     picture: String
+    # post: Post
+    # hobby: Hobby
+    # friend: Friend
+    updatedAt: String!
   }
 
-  type Post {
-    id: ID!
-    content: String!
-  }
-
-  type LoginReturn {
+  type UpdateUserReturn {
+    error: String
     user: User
-    token: String
-    error: Error
+    id: ID
   }
 
-  type Error {
-    message: String
+  input LoginUserInput {
+    email: String!
+    password: String!
+  }
+
+  type LoginUserReturn {
+    error: String
+    user: User
+    id: ID
+  }
+
+  input CreatePostInput {
+    content: String!
+    userId: ID!
+    createdAt: String!
+  }
+
+  type CreatePostReturn {
+    error: String
+    post: Post
+    id: ID
+  }
+
+  input UpdatePostInput {
+    content: String
+    updatedAt: String!
+  }
+
+  type UpdatePostReturn {
+    error: String
+    post: Post
+    id: ID
+  }
+
+  input FriendRequestInput {
+    recipientId: ID!
+    createdAt: String!
+  }
+
+  type FriendRequestReturn {
+    error: String
+    friend: Friend
+    id: ID
   }
 `
