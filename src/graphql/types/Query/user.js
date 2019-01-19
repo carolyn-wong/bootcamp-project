66c39b867bf257c87b1c384872af1c0da7fcf858
Carolyn Wong <carolynwong@college.harvard.edu>
2019-01-18 19:05:11 -0500

    added resolvers

--- src/graphql/types/Query/user.js
+++ src/graphql/types/Query/user.js
@@ -1,28 +1,56 @@
 const User = require('../../../models/User')
+const Post = require('../../../models/Post')
+const Hobby = require('../../../models/Hobby')
 
 const userResolver = async (obj, args, context) => {
-  // TODO: Write a resolver which returns a user given a user id.
+  const user = await User.query().findById(args.id)
+  return user
 }
 
 const usersResolver = async (obj, args, context) => {
-  const { substr, hometown, house, concentration, hobbies } = args
-  /* TODO: Write a resolver which returns a list of all users.
+  const { name, hometown, house, concentration, hobbies } = args
+  const users = await User.query().where(qb => {
+    if (name) {
+      qb.where('name', 'like', `%${name}%`)
+    }
 
-  Once you're done, implement the following pieces of functionality one by one:
+    if (hometown) {
+      qb.where('hometown', '=', hometown)
+    }
 
-  If any of the following arguments are provided, apply the corresponding filter:
-    - substr: include only users whose name contains the substring
-    - hometown: include only users from that hometown
-    - house: include only users from that house
-    - concentration: include only users who have that concentration
-    - hobbies: include only users who have indicated one of the hobbies in that list
-  */
+    if (house) {
+      qb.where('house', '=', house)
+    }
+
+    if (concentration) {
+      qb.where('concentration', '=', concentration)
+    }
+
+    if (hobbies) {
+      qb.where('hobbies', '=', hobbies)
+    }
+  })
+  return users
+}
+
+const userPostResolver = async (obj, args, context) => {
+  const userPosts = await Post.query().where({ userId: obj.id })
+  return userPosts
+}
+
+const userHobbyResolver = async (obj, args, context) => {
+  const userHobbies = await Hobby.query().where({ userId: obj.id })
+  return userHobbies
 }
 
 const resolver = {
   Query: {
     user: userResolver,
     users: usersResolver,
+  },
+  User: {
+    post: userPostResolver,
+    hobby: userHobbyResolver,
   },
 }
 
